b81e6ea597385e53408ad5cb37c142f146feef06
JonesHenrik <henrikjones22@gmail.com>
2023-10-25 11:21:10 -0400

    Updated

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,9 +6,18 @@
 import Foundation
 
 let chapterTitle = "Climbing Adventure"
-
+let isClimbing = true
 
 func chapterThree() {
 print("Welcome to chapter 3 the \(chapterTitle)")
 print("Time for me to begin my journey!")
+    
+    if isClimbing == true {
+        climbing()
+    }
+    
+}
+
+func climbing() {
+    print("WOOT WOOT, I can't belive I am climbing right now!")
 }
